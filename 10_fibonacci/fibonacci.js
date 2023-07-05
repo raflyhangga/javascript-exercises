913f215c5b78f4da0a9a965a7b6c4ed33937d4c6
Rafly Hanggaraksa <ajarafly422@gmail.com>
2023-07-05 12:01:18 +0700

    finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let value = Number(a);
+    if (value<0) {
+        return "OOPS"
+    } else {
+        if (value <= 1){
+            return value;
+        } else {
+            return fibonacci(value-1) + fibonacci(value-2);
+        }
+    }
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = people.sort((personBefore,personAfter) => {
+        if(!personBefore.yearOfDeath) {
+            personBefore.yearOfDeath = new Date().getFullYear();
+        }
+        if(!personAfter.yearOfDeath) {
+            personAfter.yearOfDeath = new Date().getFullYear();
+        }
 
+
+        let ageBefore = personBefore.yearOfDeath - personBefore.yearOfBirth;
+        let ageAfter = personAfter.yearOfDeath - personAfter.yearOfBirth;
+
+        if (ageBefore > ageAfter) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    return age[0];
 };
 
 // Do not edit below this line
